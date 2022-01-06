01b52081a25b0612ccb85f0f6ef16daa804d7099
praise <oluwadhammieh@gmail.com>
2022-01-06 18:40:55 +0100

    Updated

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,19 @@
 "use strict";
-// import express from 'express';
-// import mysql from 'mysql';
-// const app = express();
-// const port =process.env.PORT ||3000;
-// app.listen(port, () => {
-//     console.log('App is listening on port ' + port);
-// });
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+app.get('/api/characters', function (req, res) {
+    res.send('');
+});
+app.get('/api/characters/:id', function (req, res) {
+    var id = req.params.id;
+    res.send('' + id);
+});
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('App is listening on port ' + port);
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 import mysql from 'mysql';
 
 const app = express();
+
+const connectionString = process.env.DATABASE_URL || '';
+
+const connection = mysql.createConnection(connectionString);
+
+connection.connect()
+
+app.get('/api/characters', (req: Request, res: Response) => {
+    const query = 'SELECT * FROM Character'
+    res.send('')
+})
+
+app.get('/api/characters/:id', (req: Request, res: Response) => {
+    const id = req.params.id;
+    res.send(''  + id)
+})
 
 const port =process.env.PORT ||3000;
 
 app.listen(port, () => {
     console.log('App is listening on port ' + port);
     
-});
+}); 
